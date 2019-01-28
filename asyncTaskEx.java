3bbe024aedc7b8064162da64bc14246f56ae62a8
THOUFEEQ HIDAYATH <77thoufeeq77@gmail.com>
2019-01-28 16:21:18 +0530

    Create asyncTaskEx.java

---
+++ asyncTaskEx.java
@@ -0,0 +1,19 @@
+ 
+  public class httpManage extends AppCompatActivity {
+    Button buttons;
+    EditText putText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_http_manage);
+    }
+
+    public void doOn(View view)
+    {
+ //Asyc class name      //this is used for reference
+       getf getf = new getf(this);
+        getf.execute();
+        
+        }
+        }
